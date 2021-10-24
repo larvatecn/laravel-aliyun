1201873a18ebb5039bc27260be8c60d40b3da09e
Larva Tech <support@larvacent.com>
2021-10-24 13:07:46 +0800

    test

--- src/AliyunHelper.php
+++ src/AliyunHelper.php
@@ -8,6 +8,10 @@
 
 namespace Larva\Aliyun;
 
+use AlibabaCloud\Client\Exception\ClientException;
+use AlibabaCloud\Client\Exception\ServerException;
+use AlibabaCloud\Snsuapi\Snsuapi;
+
 /**
  * 快速操作助手
  * @author Tongle Xu <xutongle@gmail.com>
@@ -15,4 +19,17 @@ namespace Larva\Aliyun;
 class AliyunHelper
 {
 
+    /**
+     * 宽带提速预检查
+     * @param string $ipAddress IP地址
+     * @param int $port 端口
+     * @return \AlibabaCloud\Client\Result\Result
+     * @throws ClientException
+     * @throws ServerException
+     */
+    public static function SnsuBandPreCheck(string $ipAddress, int $port)
+    {
+        return Snsuapi::v20180709()->bandPrecheck()->withIpAddress($ipAddress)->withPort($port)
+            ->request();
+    }
 }
